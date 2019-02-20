117c52971077b4f0195bd70bd4174b5038ccef6b
rahul-srivastava <rahul4ror@gmail.com>
2019-02-20 12:36:16 +0530

    Added after initialize callback to add default value in portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_title, -> {where(subtitle: "Ruby on Rails")}
+
+  after_initialize :set_default_image
+
+  def set_default_image
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
 end
