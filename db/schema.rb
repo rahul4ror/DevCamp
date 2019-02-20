17976b8cd1351ed526e5e9a5199527f55b31f5da
rahul-srivastava <rahul4ror@gmail.com>
2019-02-20 14:30:12 +0530

    Added concern for default image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_055357) do
+ActiveRecord::Schema.define(version: 2019_02_20_075738) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_02_20_055357) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
